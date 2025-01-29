bffa051b4218e8d355bb036eec98a196fd35170a
HYUNSANGJIN <hsj5493@naver.com>
2025-01-29 13:11:02 +0900

    Initial Commit

---
+++ HelloPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, Swift"
+
+print(greeting)
+//거터(Gutter)
+//코드를 입력한 라인까지 실행을 시켜줌
+
+//디버그 영역(Debug Area)
+//코드 실행 시 값이 노출되는 영역
+//콘솔(Consol) > 디버그 영역에서 결과를 출력하는 부분
+
+print(greeting)
+
+UIImage(systemName: "star")
+
+var sum = 0
+for i in 1...10{
+    sum += i
+}
+print(sum)
